3d287442c95a46cdd5d29b27cb6fdfc942a4419c
LichRich <leech135789@gmail.com>
2020-11-10 16:46:46 +0900

    full-page-scroll 참고: https://www.cssscript.com/cross-platform-smooth-one-page-scrolling-pure-javascript/

--- js/common.js
+++ js/common.js
@@ -1,4 +1,27 @@
-let logo = document.getElementById('logo');
+/*
+ *
+ * header
+ *
+ */
+var logo = document.getElementById('logo');
 logo.onclick = function() {
   location.href = "file:///C:/Users/leech/Desktop/program/study/WordEB/html/index.html";
 }
+var login = document.getElementById('login');
+login.onclick = function() {
+  location.href = "file:///C:/Users/leech/Desktop/program/study/WordEB/html/client/login.html";
+}
+var myPage = document.getElementById('myPage');
+myPage.onclick = function() {
+  location.href = "file:///C:/Users/leech/Desktop/program/study/WordEB/html/client/myPage.html";
+}
+
+/*
+ *
+ * nav
+ *
+ */
+ var go_trans = document.getElementById('go_trans');
+ go_trans.onclick = function() {
+   location.href = "file:///C:Users/leech/Desktop/program/study/WordEB/html/trans/translation.html";
+ }
---
+++ js/index.js
@@ -0,0 +1,14 @@
+var skip = document.getElementById('skip');
+skip.onclick = function() {
+  location.href = "file:///C:Users/leech/Desktop/program/study/WordEB/html/voca/voca_list.html";
+}
+
+var go_voca_intro = document.getElementById('go_voca_intro');
+go_voca_intro.onclick = function() {
+  location.href = "file:///C:Users/leech/Desktop/program/study/WordEB/html/voca/voca_intro.html";
+}
+
+var go_trans_intro = document.getElementById('go_trans_intro');
+go_trans_intro.onclick = function() {
+  location.href = "file:///C:Users/leech/Desktop/program/study/WordEB/html/trans/trans_intro.html";
+}
