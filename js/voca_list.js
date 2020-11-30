95ce1819fb7e0be8a8d63a6d3bed28d368f57b62
LichRich <leech135789@gmail.com>
2020-12-01 00:34:08 +0900

    vocabulary save 기능 구현 1. load만 잘 해오면 될 듯

--- js/voca_list.js
+++ js/voca_list.js
@@ -8,6 +8,36 @@ logo.onclick = function() {
     location.href = "../../html/index.html";
 }
 
+const load_voca = document.getElementById('load_voca');
+const voca_list = document.querySelector('.voca_list');
+load_voca.addEventListener('change', showTextFile);
+function showTextFile() {
+  const selectedFiles = load_voca.files;
+  const list = document.createElement('ul');
+  voca_list.appendChild(list);
+  for(const file of selectedFiles) {
+    const listItem = document.createElement('li');
+    if(validFileType(file)) {
+      const name = document.createElement('div');
+      var str_name = `${file.name}`;
+      name.textContent = str_name.slice(0,-4);
+      const textContents = document.createElement('div');
+      listItem.appendChild(name);
+    } else {
+      const message = document.createElement('div');
+      message.textContent = `파일명 ${file.name}: .txt 파일을 선택하세요`;
+      listItem.appendChild(message);
+    }
+    list.appendChild(listItem);
+  }
+}
+const fileTypes = [
+'text/plain',
+];
+function validFileType(file) {
+return fileTypes.includes(file.type);
+}
+
 /*
  *
  * footer
--- js/vocabulary.js
+++ js/vocabulary.js
@@ -7,6 +7,40 @@ var logo = document.getElementById('logo');
 logo.onclick = function() {
     location.href = "../../html/index.html";
 }
+var back = document.getElementById('div_title');
+back.onclick = function() {
+    location.href = "../../html/voca/voca_list.html";
+}
+
+/*
+ *
+ * sections
+ *
+ */
+// 새 단어 추가
+// function saveToFile(fileName, content) {
+//     var blob = new Blob([content], { type: 'text/plain' });
+//     objURL = window.URL.createObjectURL(blob);
+//
+//     // 이전에 생성된 메모리 해제
+//     if (window.__Xr_objURL_forCreatingFile__) {
+//         window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
+//     }
+//     window.__Xr_objURL_forCreatingFile__ = objURL;
+//     var a = document.createElement('a');
+//     a.download = fileName;
+//     a.href = objURL;
+//     a.click();
+// }
+function saveAsFile(str, filename) {
+  var filename = "output.txt";
+  var str = document.getElementById("txt_newVoca").value;
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(str);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = filename;
+  hiddenElement.click();
+}
 
 /*
  *
